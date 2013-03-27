ac2a741fe1747e9d833e09656fcfb250c0777575
Snow Hellsing <snow.hellsing@gmail.com>
2013-03-27 17:31:06 +0800

    CHG: use `Rails::VERSION:MAJOR` instead of `Rails.version.to_i` because there version string like "4.0.0.beta1"

--- lib/better_logging/better_logging.rb
+++ lib/better_logging/better_logging.rb
@@ -2,26 +2,26 @@
 # logging format. See the README file for more info.
 #
 # This is distributed under a Creative Commons "Attribution-Share Alike"
-# license: for details see: 
+# license: for details see:
 # http://creativecommons.org/licenses/by-sa/3.0/
 #
 module PaulDowman
   module RailsPlugins
     module BetterLogging
-      
+
       LENGTH = ActiveSupport::Logger::Severity.constants.map{|c| c.to_s.length}.max
-      
+
       def self.included(base)
         base.class_eval do
           alias_method_chain :add, :extra_info
           alias_method_chain :error, :exception_param
           alias_method_chain :warn, :exception_param
         end
-        
+
         # Get the length to format the output so that the pid column lines up.
         # The severity levels probably won't change but this avoids hard-coding
         # them anyway, just in case.
-        # Most of this is done with class_eval so it should only be done once 
+        # Most of this is done with class_eval so it should only be done once
         # while the class is being loaded.
         if_stmts = ""
         for c in ActiveSupport::Logger::Severity.constants
@@ -29,7 +29,7 @@ module PaulDowman
             if severity == #{c}
               severity_name = sprintf("%1$*2$s", "#{c}", #{LENGTH * -1})
               use_colour = false
-              if Rails.version.to_i >= 3
+              if Rails::VERSION::MAJOR >= 3
                 use_colour = true if ActiveSupport::LogSubscriber.colorize_logging
               else
                 use_colour = true if defined?(ActiveRecord) && ActiveRecord::Base.colorize_logging
@@ -54,7 +54,7 @@ module PaulDowman
           end
         EOT
       end
-      
+
       def self.fraction_digits=(num)
         @@fraction_digits = num
       end
@@ -62,33 +62,33 @@ module PaulDowman
       def self.verbose=(boolean)
         @@verbose = boolean
       end
-      
+
       def self.custom=(string)
         @@custom = string
         @@line_prefix = format_line_prefix
       end
-      
+
       def self.hostname_maxlen=(integer)
         @@hostname_maxlen = integer
         @@line_prefix = format_line_prefix
       end
-      
+
       def self.format_line_prefix
         if @@full_hostname.length < @@hostname_maxlen
           hostname = @@full_hostname
         else
           hostname = @@full_hostname[-(@@hostname_maxlen)..-1]
         end
-        
+
         line_prefix = sprintf("%1$*2$s", "#{hostname}.#{@@pid}  ", -(7 + hostname.length))
         line_prefix = "#{@@custom}  #{line_prefix}" if @@custom
         return line_prefix
       end
-      
+
       def self.get_hostname
         `hostname -s`.strip
       end
-      
+
       # The following are cached as class variables for speed.
 
       # These are configurable, put something like the following in an initializer:
@@ -99,12 +99,12 @@ module PaulDowman
       @@custom = nil
       @@fraction_digits = 6
 
-      
+
       # These are not configurable
       @@pid = $$
       @@line_prefix = format_line_prefix
 
-            
+
       # the cached pid can be wrong after a fork(), this checks if it has changed and
       # re-caches the line_prefix
       def update_pid
@@ -113,7 +113,7 @@ module PaulDowman
           @@line_prefix = BetterLogging.format_line_prefix
         end
       end
-      
+
       def add_with_extra_info(severity, message = nil, progname = nil, &block)
         update_pid
         time = @@verbose ? "#{Time.now.iso8601(@@fraction_digits)} " : ""
@@ -122,29 +122,29 @@ module PaulDowman
           if block_given?
             if severity < @level
               return true
-            end        
+            end
             message = yield
           end
         end
         message = "#{time}#{ActiveSupport::Logger.severity_name(severity)} #{message}"
-        
-        # Make sure every line has the PID and hostname and custom string 
+
+        # Make sure every line has the PID and hostname and custom string
         # so we can use grep to isolate output from one process or server.
         # gsub works even when the output contains "\n", though there's
         # probably a small performance cost.
         message = message.gsub(/^/, @@line_prefix) if @@verbose
-        
+
         add_without_extra_info(severity, message, progname, &block)
       end
-      
-      
+
+
       # add an optional second parameter to the error & warn methods to allow a stack trace:
-            
+
       def error_with_exception_param(message, exception = nil)
         message += "\n#{exception.inspect}\n#{exception.backtrace.join("\n")}" if exception
         error_without_exception_param(message)
       end
-      
+
       def warn_with_exception_param(message, exception = nil)
         message += "\n#{exception.inspect}\n#{exception.backtrace.join("\n")}" if exception
         warn_without_exception_param(message)
